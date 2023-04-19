53c0eea2e92de0bee8db22c235bcb29d64e360ec
Julien <julien.bonyadi@gmail.com>
2023-04-19 15:21:15 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch');
+const movieDBkey = process.env.movieDBkey
+
+
+router.get('/movies', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${movieDBkey}`)
+   .then(response => response.json())
+   .then(data => {
+    res.json({ movies: data.results });
+   });
+})
+
+
+module.exports = router;
+
+
+
+
+
